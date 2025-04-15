7820908a346dfc256adbd4b5f406343b255b6f90
Natalie <nataly.senyk@gmail.com>
2025-04-15 13:05:29 +0300

    removed unused imports

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
 import '@/assets/styles/globals.css';
-import Header from '@/components/shared/header';
-import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
 	title: 'Prostore',
